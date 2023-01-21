a0f4e7f0b8cf51e1f1aa070af2064c7b4c4a9522
simueljester <68948155+simueljester@users.noreply.github.com>
2023-01-21 14:02:55 +0800

    initial commit

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+   <div class="card shadow-sm border-custom">
+    <div class="card-body">
+        Users
+    </div>
+   </div>
+</div>
+@endsection
