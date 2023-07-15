e3c8b42119abac389dc6f9f762d016ce96e79a2f
Jester <simueljester@gmail.com>
2023-07-15 14:58:51 +0800

    dynamic css call

--- resources/views/layouts/app-soft-ui.blade.php
+++ resources/views/layouts/app-soft-ui.blade.php
@@ -26,13 +26,13 @@
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
+  <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{asset('assets/css/nucleo-svg.css')}}" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.7" rel="stylesheet" />
+  <link id="pagestyle" href="{{asset('assets/css/soft-ui-dashboard.css')}}" rel="stylesheet" />
   <!-- Nepcha Analytics (nepcha.com) -->
   <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
   <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
@@ -117,25 +117,11 @@
       </div>
     </nav>
 {{-- End Nav Bar --}}
+
+
     <div class="container-fluid py-4">
 {{-- Yield --}}
-      @if(session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <span class="alert-icon"><i class="ni ni-like-2"></i></span>
-            <span class="alert-text"><strong>Success!</strong> {{ session()->get('success') }}</span>
-        </div>
-      @endif
-        @if(session()->has('error'))
-        <div class="alert alert-danger" role="alert">
-            <i class="fas fa-exclamation-triangle"></i> <strong>Notice!</strong>  {{session()->get('error') }}
-        </div>
-        @endif
-      @if ($errors->any())
-        @foreach ($errors->all() as $error)
-            <div class="alert alert-danger" role="alert">{{$error}}</div>
-        @endforeach
-      @endif
-      @yield('content')
+        @yield('content')
 {{-- End Yield --}}
 
       <footer class="footer pt-3  ">
