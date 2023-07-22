5564a7e33d50e3918a4ef0daa7425efff5bf39bf
Jester <simueljester@gmail.com>
2023-07-22 12:42:44 +0800

    update

---
+++ resources/views/page/landing/about-events.blade.php
@@ -0,0 +1,48 @@
+
+<div class="container-fluid bg-white p-5">
+   <div class="row ">
+    <div class="col-sm-6 ">
+        <div>
+          
+            <h2> <b> About TSP </b>  </h2>
+            <p class="text-muted">
+                Nulla quis lorem ut libero malesuada feugiat. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <center>
+                <img class="about" width="250" src="{{ asset('images') }}/tsp3.png"> 
+            </center>
+        </div>
+      
+    </div>
+    <div class="col-sm-6 text-left">
+        <h2> <b> News & Events </b> </h2>
+        <div class="column small-11 small-centered">
+			<div class="slider slider-single">
+				<div><h3>1a</h3></div>
+				<div><h3>2</h3></div>
+				<div><h3>3</h3></div>
+				<div><h3>4</h3></div>
+				<div><h3>5</h3></div>
+				<div><h3>6</h3></div>
+				<div><h3>7</h3></div>
+				<div><h3>8</h3></div>
+				<div><h3>9</h3></div>
+				<div><h3>10</h3></div>
+			</div>
+			<div class="slider slider-nav">
+				<div><h3><span>12</span></h3></div>
+				<div><h3><span>2</span></h3></div>
+				<div><h3><span>3</span></h3></div>
+				<div><h3><span>4</span></h3></div>
+				<div><h3><span>5</span></h3></div>
+				<div><h3><span>6</span></h3></div>
+				<div><h3><span>7</span></h3></div>
+				<div><h3><span>8</span></h3></div>
+				<div><h3><span>9</span></h3></div>
+				<div><h3><span>10</span></h3></div>
+			</div>
+		</div>
+
+    </div>
+   </div>
+</div>
--- resources/views/page/landing/index.blade.php
+++ resources/views/page/landing/index.blade.php
@@ -49,7 +49,7 @@
     @include('page.landing.service')
     @include('page.landing.articles')
     {{-- @include('page.landing.brands') --}}
-    @include('page.landing.about')
+    @include('page.landing.about-events')
     @include('page.landing.choose-us')
     @include('page.landing.contact')
     @include('page.landing.footer')
@@ -58,6 +58,7 @@
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script type="text/javascript">
     $(document).ready(function(){
+        // Slick Start
         $('.single-item').slick({
             autoplay:true,
             arrows:false,
@@ -93,14 +94,66 @@
                 }
             ]
         });
-     
 
-        $('.single-item').slick({
-            autoplay:true,
-            fade:true,
+        $('.slider-single').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: false,
             adaptiveHeight: true,
-            dots: true,
+            infinite: false,
+            useTransform: true,
+            speed: 400,
+            cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
         });
+
+    $('.slider-nav')
+ 	.on('init', function(event, slick) {
+ 		$('.slider-nav .slick-slide.slick-current').addClass('is-active');
+ 	})
+ 	.slick({
+ 		slidesToShow: 7,
+ 		slidesToScroll: 7,
+ 		dots: false,
+ 		focusOnSelect: false,
+ 		infinite: false,
+ 		responsive: [{
+ 			breakpoint: 1024,
+ 			settings: {
+ 				slidesToShow: 5,
+ 				slidesToScroll: 5,
+ 			}
+ 		}, {
+ 			breakpoint: 640,
+ 			settings: {
+ 				slidesToShow: 4,
+ 				slidesToScroll: 4,
+			}
+ 		}, {
+ 			breakpoint: 420,
+ 			settings: {
+ 				slidesToShow: 3,
+ 				slidesToScroll: 3,
+		    }
+ 		}]
+ 	});
+
+    $('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+        $('.slider-nav').slick('slickGoTo', currentSlide);
+        var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+        $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+        $(currrentNavSlideElem).addClass('is-active');
+    });
+
+    $('.slider-nav').on('click', '.slick-slide', function(event) {
+        event.preventDefault();
+        var goToSingleSlide = $(this).data('slick-index');
+
+        $('.slider-single').slick('slickGoTo', goToSingleSlide);
+    });
+        // Slick End
+
+      
     });
 
     $(window).on('scroll', function (e) {
